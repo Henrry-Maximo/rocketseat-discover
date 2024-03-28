c2e85ef261a5420637a0f0046d5bdf4a3199eabd
Henrry-Maximo <Henrrylimadasilva@gmail.com>
2024-03-28 12:32:53 -0300

    Rocketseat: Projeto Discover

---
+++ script.js
@@ -0,0 +1,25 @@
+// função para executar método quando o botão for pressionado
+function toggleMode() {
+  const html = document.documentElement;
+  html.classList.toggle('light');
+
+  // selecionar o elemento através da class para obter a imagem
+  const img = document.querySelector("#profile img");
+
+  if (html.classList.contains('light')) {
+    // utilizar o atributo "setAttribute" para especificar o elemento que deve ser alterado
+    // inserir dois parâmetros: qual e onde...
+    img.setAttribute("src", "./assets/img/moon-stars.svg")
+    img.setAttribute("alt", "teste")
+  } else {
+    img.setAttribute("src", "./assets/img/sun.svg")
+    img.setAttribute("alt", "teste 2")
+  }
+
+  // classList é um objeto que contém método, um dele é pra verificar se existe
+  // if (html.classList.contains('light')) {
+  //   html.classList.remove('light');
+  // } else {
+  //   html.classList.add('light');
+  // }
+}
